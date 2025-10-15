28cf426ca074462820ade1cae9bbd939bda6946f
Jean Carlo Nascimento <jnascimento@gmail.com>
2025-10-15 11:36:28 -0300

    Update hello.ts

--- src/api/hello.ts
+++ src/api/hello.ts
@@ -1,3 +1,12 @@
-export default async function handler({ req }) {
-  return { message: 'Hello from Vite API!', method: req.method }
+
+export default async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url)
+  const params = Object.fromEntries(url.searchParams.entries())
+
+  return Response.json({
+    message: "Olá do Atomic Behavior Routes 🚀",
+    method: req.method,
+    params,
+    time: new Date().toISOString(),
+  })
 }
